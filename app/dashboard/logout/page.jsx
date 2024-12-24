dd53199429f4fad6334e0f5e7a0749bac0f09e2a
Arsh_Singh <arshdeepsingh92283@gmail.com>
2024-12-24 17:37:36 +0530

    Updated Logout component and Clerk Provider

--- app/dashboard/logout/page.jsx
+++ app/dashboard/logout/page.jsx
@@ -1,25 +1,23 @@
 "use client";
-
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
-import { signOut } from "@clerk/nextjs";
-
+import { useAuth } from "@clerk/nextjs"; 
 function Logout() {
   const router = useRouter();
+  const { signOut } = useAuth(); 
 
   useEffect(() => {
-    // Perform sign out using Clerk
     const handleLogout = async () => {
       try {
-        await signOut(); // Clerk sign-out method
-        router.push("/"); // Redirect to the landing page
+        await signOut(); 
+        router.push("/"); 
       } catch (error) {
         console.error("Error during logout:", error);
       }
     };
 
     handleLogout();
-  }, [router]);
+  }, [router, signOut]);
 
   return (
     <div className="flex justify-center items-center h-screen">
--- app/layout.js
+++ app/layout.js
@@ -23,7 +23,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ClerkProvider>
+        <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
           <GoogleOneTap />
           {children}
         </ClerkProvider>
